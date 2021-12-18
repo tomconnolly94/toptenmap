d075bda9bd9da3a3b501e5535d3edb8a195b1674
Tom <tom.connolly@protonmail.com>
2021-12-18 18:48:36 +0100

    Initialised React app, removed page serving from flask app

--- app-container/api/app.py
+++ app-container/api/app.py
@@ -1,7 +1,7 @@
 #!/venv/bin/python
 
 # external dependencies
-from flask import Flask, request, Response, send_from_directory
+from flask import Flask, request, Response, render_template
 from dotenv import load_dotenv
 from os.path import join, dirname
 import os
@@ -9,23 +9,22 @@ import json
 
 # internal dependencies
 #create app
-app = Flask(__name__, template_folder="client")
+app = Flask(__name__, template_folder="../client")
 
 # load env file
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-def getResponse(errorCode, errorMessage):
-    return Response(errorMessage, status=errorCode, mimetype="text/html")
+host = "http://localhost:80" if os.getenv("ENVIRONMENT") == "development" else ""
 
-def serveIndex():
-    f = open(f"../client/index.html", "r")
-    return f.read()
+def getResponse(responseCode, responseMessage):
+    return Response(responseMessage, status=responseCode, mimetype="application/json")
 
 
-@app.route("/", methods=["GET"])
+# api routes
+@app.route("/locations", methods=["GET"])
 def index():
-    return serveIndex()
+    return getResponse(200, "nuffin")
 
 
 
